ccea7610656af865093b06340d39296a625f88a2
jzhuChoice <jzhu@choice.com.au>
2020-04-04 17:19:29 +1100

    add recipe model and recipe list page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cookieandkate.com/images/2020/03/vegan-chana-masala-recipe-2.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cookieandkate.com/images/2020/03/vegan-chana-masala-recipe-2.jpg')
+  ];
 
   constructor() { }
 

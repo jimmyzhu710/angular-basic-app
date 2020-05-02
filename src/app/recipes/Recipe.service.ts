81c24b18a6bf2195e5d8649686a2319562b83d84
Jimmy <jzhu@choice.com.au>
2020-05-02 19:20:08 +1000

    179. Replace event emitter with subject

--- src/app/recipes/Recipe.service.ts
+++ src/app/recipes/Recipe.service.ts
@@ -1,13 +1,14 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
 
-    private recipes: Recipe[] = [
+  private recipes: Recipe[] = [
         new Recipe(
           'Tasty Schnitzel',
           'A super-tasty Schnitzel - just awesome!',

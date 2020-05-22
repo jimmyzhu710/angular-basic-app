9c43645b73dc9eebecb4304709a8a119382a3473
Jimmy <jzhu@choice.com.au>
2020-05-22 22:24:40 +1000

    315. Data Binding & Event Binding

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
-import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from './auth.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { AuthResponseData } from './auth.service';
 import { Router } from '@angular/router';
 import { AlertComponent } from '../shared/alert/alert.component';
@@ -12,21 +12,23 @@ import { PlaceholderDirective } from '../shared/placeholder/placeholder.directiv
     templateUrl: './auth.component.html'
 })
 
-export class AuthComponent{
+export class AuthComponent implements OnDestroy {
     isLoginMode = true;
     isLoading = false;
     error: string = null;
     @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
-    
-    constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver){}
 
-    onSwitchMode(){
+    private closeSub: Subscription;
+
+    constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) { }
+
+    onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
     }
 
-    
-    onSubmit(form: NgForm){
-        if(!form.valid){
+
+    onSubmit(form: NgForm) {
+        if (!form.valid) {
             return;
         }
         const email = form.value.email;
@@ -35,43 +37,54 @@ export class AuthComponent{
         let authObs: Observable<AuthResponseData>
 
         this.isLoading = true;
-        if(this.isLoginMode){
+        if (this.isLoginMode) {
             authObs = this.authService.login(email, password);
-        }else{
+        } else {
             authObs = this.authService.signup(email, password);
         }
 
         authObs.subscribe(
-            resData =>{
+            resData => {
                 console.log(resData);
                 this.isLoading = false;
                 this.router.navigate(['/recipes']);
             },
-            errorMessage=>{
+            errorMessage => {
                 console.log(errorMessage);
-               this.error = errorMessage;
-               this.showErrorAlert(errorMessage);
+                this.error = errorMessage;
+                this.showErrorAlert(errorMessage);
                 this.isLoading = false;
-            });            
+            });
 
 
         form.reset();
     }
 
-    onHandleError(){
+    onHandleError() {
         this.error = null;
     }
 
+    ngOnDestroy() {
+        if (this.closeSub) {
+            this.closeSub.unsubscribe();
+        }
+    }
 
     private showErrorAlert(message: string) {
         // const alertCmp = new AlertComponent();
         const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(
-          AlertComponent
+            AlertComponent
         );
         const hostViewContainerRef = this.alertHost.viewContainer;
         hostViewContainerRef.clear();
-    
+
         const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
-    
-      }
+
+        componentRef.instance.message = message;
+        this.closeSub = componentRef.instance.close.subscribe(() => {
+            this.closeSub.unsubscribe();
+            hostViewContainerRef.clear();
+        })
+    }
+
 }

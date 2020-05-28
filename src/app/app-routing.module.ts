2ee8e77589e4298f090f8359d3dfaf3c262deb61
Jimmy <jzhu@choice.com.au>
2020-05-28 18:51:21 +1000

    325. The ShoppingList Feature Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from '@angular/core';
-import { RecipesComponent } from './recipes/recipes.component';
 import { Routes, RouterModule } from '@angular/router';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthGuard } from './auth/auth.guard';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'shopping-list', component: ShoppingListComponent },
     { path: 'auth', component: AuthComponent}
 ];
 
---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+    declarations: [
+        ShoppingEditComponent,
+        ShoppingListComponent
+    ],
+    imports: [CommonModule, FormsModule, RouterModule.forChild([
+        { path: 'shopping-list', component: ShoppingListComponent }
+    ])]
+})
+export class ShoppingListModule { }

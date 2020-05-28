0585596fa8414b2d192ed1d78309110a8984bdd0
Jimmy <jzhu@choice.com.au>
2020-05-28 18:12:10 +1000

    324. Component Declarations

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -21,14 +21,6 @@ import { RecipesRoutingModule } from './recipes-routing.module';
         RecipeEditComponent
     ],
     imports: [RouterModule, CommonModule, ReactiveFormsModule, RecipesRoutingModule],
-    exports: [
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeItemComponent,
-        RecipeStartComponent,
-        RecipeEditComponent
-    ]
 })
 
 export class RecipesModule{}

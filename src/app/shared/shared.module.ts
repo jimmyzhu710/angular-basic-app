a13a62b6ebeb4d9378129f1a2ed207046461d181
Jimmy <jzhu@choice.com.au>
2020-05-29 10:36:32 +1000

    326. Adding Shared Modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { AlertComponent } from './alert/alert.component';
+import { PlaceholderDirective } from './placeholder/placeholder.directive';
+import { DropdownDirective } from './dropdown.directive';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [
+        AlertComponent,
+        LoadingSpinnerComponent,
+        PlaceholderDirective,
+        DropdownDirective
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports:[
+        AlertComponent,
+        LoadingSpinnerComponent,
+        PlaceholderDirective,
+        DropdownDirective,
+        CommonModule
+    ]
+})
+export class SharedModule{
+
+}
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list.component';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [
         ShoppingEditComponent,
         ShoppingListComponent
     ],
-    imports: [CommonModule, FormsModule, RouterModule.forChild([
-        { path: 'shopping-list', component: ShoppingListComponent }
-    ])]
+    imports: [
+        FormsModule,
+        RouterModule.forChild([
+            { path: 'shopping-list', component: ShoppingListComponent }
+        ]),
+        SharedModule
+    ]       
 })
 export class ShoppingListModule { }

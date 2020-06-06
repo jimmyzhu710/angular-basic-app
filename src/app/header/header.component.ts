68d396ba084d9ac0b3ae56fafc164d425ed054ef
Jimmy <jzhu@choice.com.au>
2020-06-06 21:53:49 +1000

    362, 363 Dispatch auth atcions

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,10 @@
+import { Store } from '@ngrx/store';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
+import * as fromApp from '../store/app.reducer';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-header',
@@ -12,10 +15,12 @@ export class HeaderComponent implements OnInit, OnDestroy{
     isAuthenticated = false;
     private userSub: Subscription;
 
-    constructor(private dataStorageService: DataStorageService, private authService: AuthService){}
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService, private store: Store<fromApp.AppState>){}
 
     ngOnInit(){
-        this.userSub = this.authService.user.subscribe(user=>{
+        this.userSub = this.store.select('auth').pipe(
+            map(authState=> authState.user)
+            ).subscribe(user=>{
             //this.isAuthenticated = !!user;
             this.isAuthenticated = !user ? false: true;
         });
@@ -25,7 +30,7 @@ export class HeaderComponent implements OnInit, OnDestroy{
         this.authService.logout();
     }
     ngOnDestroy(){
-        this.authService.user.unsubscribe();
+        this.userSub.unsubscribe();
     }
 
     onSaveData(){

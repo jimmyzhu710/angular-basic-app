14743f158d650b37ce5a5d3df6a01a412dc9f1ce
Jimmy <jzhu@choice.com.au>
2020-06-10 13:47:59 +1000

    372, 373. Adding Signup and Further Auth Effects

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -30,32 +30,7 @@ export class AuthService {
     constructor(private http: HttpClient, private router: Router, private store: Store<fromApp.AppState>) { }
 
     //check sign in/sign up method https://firebase.google.com/docs/reference/rest/auth#section-create-email-password 
-    signup(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
-            {
-                email: email,
-                password: password,
-                returnSecureToken: true
-            })
-            .pipe(catchError(this.handleError), tap(resData => {
-                this.handleAuthentication(resData.email, resData.localId, resData.idToken,  +resData.expiresIn);
-            }
-            ));
-    }
-
-    login(email: string, password: string) {
-        return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
-            {
-                email: email,
-                password: password,
-                returnSecureToken: true
-            })
-            .pipe(catchError(this.handleError), tap(resData => {
-                this.handleAuthentication(resData.email, resData.localId, resData.idToken,  +resData.expiresIn);
-            }
-            ));
-    }
-
+    
     autoLogin() {
         const userData : {
             email: string,
@@ -70,7 +45,7 @@ export class AuthService {
 
         if(loadedUser.token){
             //this.user.next(loadedUser);
-            this.store.dispatch(new AuthActions.Login({email: loadedUser.email, userId: loadedUser.id, token: loadedUser.token, expirationDate: new Date(userData._tokenExpirationDate)}))
+            this.store.dispatch(new AuthActions.AuthenticateSuccess({email: loadedUser.email, userId: loadedUser.id, token: loadedUser.token, expirationDate: new Date(userData._tokenExpirationDate)}))
             const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
             this.autoLogout(expirationDuration);
         }
@@ -78,8 +53,7 @@ export class AuthService {
 
     logout() {
         //this.user.next(null);
-        this.store.dispatch(new AuthActions.Logout());
-        this.router.navigate(['/auth']);
+        this.store.dispatch(new AuthActions.Logout());  
         localStorage.removeItem('userData');
         if(this.tokenExpirationTimer){
             clearTimeout(this.tokenExpirationTimer);
@@ -97,7 +71,7 @@ export class AuthService {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000)
         const user = new User(email, userId, token, expirationDate);
         //this.user.next(user);
-        this.store.dispatch(new AuthActions.Login({email: email, userId: userId, token: token, expirationDate: expirationDate}));
+        this.store.dispatch(new AuthActions.AuthenticateSuccess({email: email, userId: userId, token: token, expirationDate: expirationDate}));
         this.autoLogout(expiresIn * 1000);
         localStorage.setItem('userData', JSON.stringify(user));
     }
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -18,8 +18,51 @@ export interface AuthResponseData {
     registered?: boolean
 }
 
+const handleAuthentication = (expiresIn: number, email: string, userId: string, token: string)=> {
+    const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
+    return new AuthActions.AuthenticateSuccess({email: email, userId: userId, token: token, expirationDate: expirationDate});
+};
+
+const handleError = (errorRes: any) => {
+    let errorMessage = 'An unknow error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+        return of(new AuthActions.AuthenticateFail(errorMessage)); 
+    }
+    switch (errorRes.error.error.message) {
+        case 'EMAIL_EXISTS':
+            errorMessage = 'This email exists already';
+            break;
+        case 'EMAIL_NOT_FOUND':
+            errorMessage = 'This email does not exist';
+            break;
+        case 'INVALID_PASSWORD':
+            errorMessage = 'This password is not correct.';
+            break;
+    };
+    return of(new AuthActions.AuthenticateFail(errorMessage));   
+};
+
 @Injectable()
 export class AuthEffects {
+    @Effect()
+    authSignup = this.actions$.pipe(
+        ofType(AuthActions.SIGNUP_START),
+        switchMap((signupAction: AuthActions.SignupStart) => {
+            return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
+            {
+                email: signupAction.payload.email,
+                password: signupAction.payload.password,
+                returnSecureToken: true
+            }).pipe(
+                map(restData => {
+                   return handleAuthentication(+restData.expiresIn, restData.email, restData.localId, restData.idToken);  
+                }),
+                catchError(errorRes =>{
+                  return handleError(errorRes);
+            }));
+        })
+    );
+
     @Effect()
     authLogin = this.actions$.pipe(
         ofType(AuthActions.LOGIN_START),
@@ -31,26 +74,10 @@ export class AuthEffects {
                 returnSecureToken: true
             }).pipe(
                 map(restData => {
-                    const expirationDate = new Date(new Date().getTime() + +restData.expiresIn * 1000);
-                    return new AuthActions.Login({email: restData.email, userId: restData.localId, token: restData.idToken, expirationDate: expirationDate});
+                    return handleAuthentication(+restData.expiresIn, restData.email, restData.localId, restData.idToken);  
                 }),
                 catchError(errorRes => {
-                    let errorMessage = 'An unknow error occurred!';
-                    if (!errorRes.error || !errorRes.error.error) {
-                        return of(new AuthActions.LoginFail(errorMessage)); 
-                    }
-                    switch (errorRes.error.error.message) {
-                        case 'EMAIL_EXISTS':
-                            errorMessage = 'This email exists already';
-                            break;
-                        case 'EMAIL_NOT_FOUND':
-                            errorMessage = 'This email does not exist';
-                            break;
-                        case 'INVALID_PASSWORD':
-                            errorMessage = 'This password is not correct.';
-                            break;
-                    };
-                    return of(new AuthActions.LoginFail(errorMessage));    
+                    return handleError(errorRes);
                 }));
                
         }),
@@ -58,8 +85,8 @@ export class AuthEffects {
     );
 
     @Effect({ dispatch: false })
-    authSuccess = this.actions$.pipe(
-        ofType(AuthActions.LOGIN), 
+    authRedirect = this.actions$.pipe(
+        ofType(AuthActions.AUTHENTICATE_SUCCESS, AuthActions.LOGOUT), 
         tap(() => {
           this.router.navigate(['/']);  
     }));
